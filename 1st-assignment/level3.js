ada6c475f61b4a21629e24c50aca42a76682d424
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-10-07 17:32:12 +0900

    assignment1 level3 - 서버파트 members.js 데이터를 이용해서 랜덤으로 조를 짜는 알고리즘 만들어보기

---
+++ 1st-assignment/level3.js
@@ -0,0 +1,77 @@
+// 1-st assginment level3
+// 서버파트 members.js 데이터를 이용해서 랜덤으로 조를 짜는 알고리즘 만들어보기
+
+const members = [
+    { name: "강한희", part: "Server", group: "OB" },
+    { name: "고성용", part: "Server", group: "OB" },
+    { name: "구건모", part: "Server", group: "YB" },
+    { name: "권세훈", part: "Server", group: "YB" },
+    { name: "김영권", part: "Server", group: "YB" },
+    { name: "김은지", part: "Server", group: "YB" },
+    { name: "김진욱", part: "Server", group: "YB" },
+    { name: "김희빈", part: "Server", group: "OB" },
+    { name: "남지윤", part: "Server", group: "YB" },
+    { name: "문규원", part: "Server", group: "YB" },
+    { name: "박나희", part: "Server", group: "OB" },
+    { name: "박정현", part: "Server", group: "YB" },
+    { name: "박현지", part: "Server", group: "OB" },
+    { name: "변주현", part: "Server", group: "OB" },
+    { name: "서호영", part: "Server", group: "OB" },
+    { name: "설지원", part: "Server", group: "YB" },
+    { name: "손시형", part: "Server", group: "YB" },
+    { name: "안준영", part: "Server", group: "OB" },
+    { name: "장서현", part: "Server", group: "OB" },
+    { name: "오예원", part: "Server", group: "OB" },
+    { name: "이다은", part: "Server", group: "OB" },
+    { name: "이동근", part: "Server", group: "YB" },
+    { name: "이솔", part: "Server", group: "OB" },
+    { name: "이승헌", part: "Server", group: "YB" },
+    { name: "이정은", part: "Server", group: "YB" },
+    { name: "이제준", part: "Server", group: "YB" },
+    { name: "정설희", part: "Server", group: "OB" },
+    { name: "조윤서", part: "Server", group: "OB" },
+    { name: "조재호", part: "Server", group: "YB" },
+    { name: "조찬우", part: "Server", group: "YB" },
+    { name: "주어진사랑", part: "Server", group: "YB" },
+    { name: "주효식", part: "Server", group: "YB" },
+    { name: "채정아", part: "Server", group: "OB" },
+    { name: "최영재", part: "Server", group: "OB" },
+    { name: "최유림", part: "Server", group: "YB" },
+    { name: "최진영", part: "Server", group: "YB" },
+    { name: "허유정", part: "Server", group: "YB" },
+];
+
+let YB = [];
+let OB = [];
+
+members.forEach(member => {
+    if (member.group === 'YB') {
+        YB.push(member.name);
+    } else {
+        OB.push(member.name);
+    }
+});
+
+function shuffle(array) { array.sort(() => Math.random() - 0.5); }
+shuffle(YB);
+shuffle(OB);
+
+// 한 그룹 당 5~6명
+const memberNum = 5;
+const groupNum = Math.round(members.length / memberNum);
+let groups = [];
+
+for (let i=0; i<groupNum; i++) {
+    groups.push([]);
+}
+
+for (let i=0; i<OB.length; i++) {
+    groups[i%groupNum].push(OB[i]);
+}
+let k = OB.length % groupNum;
+
+for (let i=0; i<YB.length; i++) {
+    groups[(i+k) % groupNum].push(YB[i]);
+}
+
+console.log(groups);
