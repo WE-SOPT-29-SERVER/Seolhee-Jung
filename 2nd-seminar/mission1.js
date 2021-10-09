fdb5b45fe9ff9ff8d4971b4ff1b3522dcc4f0057
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-10-09 16:00:16 +0900

    Promise -> async 바꾸기

--- 2nd-seminar/mission1.js
+++ 2nd-seminar/mission1.js
@@ -39,5 +39,15 @@ const getOB = members => {
 // 온라인이면서 OB인 멤버
 getOnline(members).then(getOB).then(console.log);
 
+getOnline(members).then((onlineMembers) => getOB(onlineMembers)).then(console.log);
+
 // YB이면서 오프라인인 멤버
 getYB(members).then(getOffline).then(console.log);
+
+const asyncFunc = async(members) => {
+    const onlineMembrs = await getOnline(members);
+    const onlineOBMembers = await getOB(onlineMembrs);
+    console.log(onlineOBMembers);
+};
+
+asyncFunc(members);
